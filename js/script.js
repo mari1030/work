23985e3105d5f887cd25c7e19acef859b935f440
mari1030 <mariyuuto1211@gmail.com>
2021-04-14 11:06:46 +0900

    JavaScript課題-4に取り組みました。

--- js/script.js
+++ js/script.js
@@ -1,12 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const task = ["掃除","買い物","散歩"];
-  let inputTask = '';
-  
-  displayTask(task);
-  inputTask = prompt('タスクを入力してください');
-  task.push(inputTask);
-  displayTask(task);
-  alert('新しいタスクを追加しました。');
+  const tasks = [
+    {'[内容]':'机を片付ける','[ジャンル]':'掃除'},
+    {'[内容]':'牛乳を買う','[ジャンル]':'買い物'},
+    {'[内容]':'散歩する','[ジャンル]':'運動'},
+  ];
+  displayTask(tasks);
+
+  let inputTask = prompt('タスクを入力してください');
+  let inputGenre = prompt('ジャンルを入力してください');
+  let newObj = {'[内容]':inputTask,'[ジャンル]':inputGenre}; 
+  tasks.push(newObj);
+
+  displayTask(tasks);
+
+  alert('新しいタスクを追加しました。')
 });
 
 function displayTask(array) {
@@ -14,5 +21,7 @@ function displayTask(array) {
   console.log('==========================');
   console.log('現在持っているタスク一覧');
   console.log('==========================');
-  array.forEach(function(element, index) {console.log(`${index}：${element}`)} );
+  array.forEach((elm, index) => {
+    console.log(`${index}：[内容]${elm["[内容]"]}、[ジャンル]${elm["[ジャンル]"]}`)    
+  })
 }
